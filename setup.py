11a8bf0e25d5cbe533a7a38bad8cc11dc830932c
GenAI-Girl <ritu.khurana2711@gmail.com>
2024-08-02 10:17:24 +0530

    second commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Raja Kocherlakota',
-    author_email= 'kraja928@gmail.com',
+    author= 'Ritu Khurana',
+    author_email= 'ritu.khurana2711@gmail.com',
     packages= find_packages(),
     install_requires = []
 
